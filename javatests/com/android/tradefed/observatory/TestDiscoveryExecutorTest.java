2effee3adcfd4abccd1af69595f653e9ce2f095c
Julien Desprez <jdesprez@google.com>
2022-10-12 18:26:21 -0700

    Handle metadata + include filters
    This actually works and find a super-set since
    metadata filters are applied after include filters
    
    Test: presubmit
    Bug: 253293669
    Change-Id: I2052df601f2dd846edbf1408f022822e9c584baf

--- javatests/com/android/tradefed/observatory/TestDiscoveryExecutorTest.java
+++ javatests/com/android/tradefed/observatory/TestDiscoveryExecutorTest.java
@@ -168,16 +168,13 @@ public class TestDiscoveryExecutorTest {
     public void testDiscoverDependencies_UnsupportedOptions() throws Exception {
         // Mock to return some include filters
         BaseTestSuite test1 = new BaseTestSuite();
-        Set<String> includeFilters1 = new HashSet<>();
-        includeFilters1.add("TestModule1 class#function1");
-        includeFilters1.add("TestModule2");
-        includeFilters1.add("x86_64 TestModule3 class#function3");
+        Set<String> emptyFilters = new HashSet<>();
 
         // Metadata include filter exist
         Map<String, String> map = new HashMap<>();
         map.put("component", "media");
         test1.addModuleMetadataIncludeFilters(new MultiMap<>(map));
-        test1.setIncludeFilter(includeFilters1);
+        test1.setIncludeFilter(emptyFilters);
 
         List<IRemoteTest> testList = new ArrayList<>();
         testList.add(test1);
@@ -198,5 +195,36 @@ public class TestDiscoveryExecutorTest {
                                     "Tradefed Observatory can't do test discovery because the"
                                             + " existence of metadata include filter option."));
         }
+    }
+
+    @Test
+    public void testDiscoverDependencies_metadataAndIncludeFilters() throws Exception {
+        // Mock to return some include filters
+        BaseTestSuite test1 = new BaseTestSuite();
+        Set<String> includeFilters1 = new HashSet<>();
+        includeFilters1.add("TestModule1 class#function1");
+        includeFilters1.add("TestModule2");
+        includeFilters1.add("x86_64 TestModule3 class#function3");
+
+        // Metadata include filter exist
+        Map<String, String> map = new HashMap<>();
+        map.put("component", "media");
+        test1.addModuleMetadataIncludeFilters(new MultiMap<>(map));
+        test1.setIncludeFilter(includeFilters1);
+
+        List<IRemoteTest> testList = new ArrayList<>();
+        testList.add(test1);
+        when(mMockedConfiguration.getTests()).thenReturn(testList);
+        List<Object> preparers = new ArrayList<>();
+        preparers.add(new DiscoverablePreparer());
+        when(mMockedConfiguration.getAllConfigurationObjectsOfType(
+                        Configuration.TARGET_PREPARER_TYPE_NAME))
+                .thenReturn(preparers);
+
+        String output = mTestDiscoveryExecutor.discoverDependencies(new String[0]);
+        String expected =
+                "{\"TestModules\":[\"TestModule1\",\"TestModule2\",\"TestModule3\"],"
+                        + "\"TestDependencies\":[\"someapk.apk\"]}";
+        assertEquals(expected, output);
     }
 }
