0c02a80586cc99f65b568f9d23fc462069f7cc3b
Julien Desprez <jdesprez@google.com>
2023-04-17 10:57:26 -0700

    Move bugreport toward non-native
    Test: presubmit
    Bug: 276495569
    Change-Id: I0dd15e8829c971d143c576584d5a4532440b6361

--- device_build_interfaces/com/android/tradefed/device/INativeDevice.java
+++ device_build_interfaces/com/android/tradefed/device/INativeDevice.java
@@ -28,7 +28,6 @@ import com.android.tradefed.log.ITestLogger;
 import com.android.tradefed.result.ITestLifeCycleReceiver;
 import com.android.tradefed.result.InputStreamSource;
 import com.android.tradefed.targetprep.TargetSetupError;
-import com.android.tradefed.util.Bugreport;
 import com.android.tradefed.util.CommandResult;
 import com.android.tradefed.util.MultiMap;
 import com.android.tradefed.util.ProcessInfo;
@@ -37,7 +36,6 @@ import com.android.tradefed.util.TimeUtil;
 import com.google.errorprone.annotations.MustBeClosed;
 
 import java.io.File;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Collection;
 import java.util.Date;
@@ -1456,46 +1454,6 @@ public interface INativeDevice {
      * @throws DeviceNotAvailableException
      */
     public String getBuildSigningKeys() throws DeviceNotAvailableException;
-
-    /**
-     * Retrieves a bugreport from the device.
-     * <p/>
-     * The implementation of this is guaranteed to continue to work on a device without an sdcard
-     * (or where the sdcard is not yet mounted).
-     *
-     * @return An {@link InputStreamSource} which will produce the bugreport contents on demand.  In
-     *         case of failure, the {@code InputStreamSource} will produce an empty
-     *         {@link InputStream}.
-     */
-    public InputStreamSource getBugreport();
-
-    /**
-     * Retrieves a bugreportz from the device. Zip format bugreport contains the main bugreport
-     * and other log files that are useful for debugging.
-     * <p/>
-     * Only supported for 'adb version' > 1.0.36
-     *
-     * @return a {@link InputStreamSource} of the zip file containing the bugreportz, return null
-     *         in case of failure.
-     */
-    public InputStreamSource getBugreportz();
-
-    /**
-     * Helper method to take a bugreport and log it to the reporters.
-     *
-     * @param dataName name under which the bugreport will be reported.
-     * @param listener an {@link ITestLogger} to log the bugreport.
-     * @return True if the logging was successful, false otherwise.
-     */
-    public boolean logBugreport(String dataName, ITestLogger listener);
-
-    /**
-     * Take a bugreport and returns it inside a {@link Bugreport} object to handle it. Return null
-     * in case of issue.
-     * </p>
-     * File referenced in the Bugreport object need to be cleaned via {@link Bugreport#close()}.
-     */
-    public Bugreport takeBugreport();
 
     /**
      * Collects and log ANRs from the device.
